7e6be47dea825a1bc085aa9ea6f8d097827a0c6d
Aishwarya1155 <aishwaryamanoharann@gmail.com>
2025-03-04 20:58:25 +0530

    using redux tool

---
+++ redux/src/store/Store.jsx
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./Slice";
+
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
+export default store;
